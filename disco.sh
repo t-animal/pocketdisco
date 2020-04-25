b562b07a2d4a86992fc8a4af6edf1fd226ee8c36
Tilman Adler <Tilman.Adler@methodpark.de>
2020-04-25 18:03:52 +0200

    Go back to using alsa

--- disco.sh
+++ disco.sh
@@ -2,35 +2,25 @@
 
 cd "$(dirname "$0")"
 
-gpio mode 8 output
 gpio mode 7 input
+gpio mode 8 output
 
-pulseaudio -D
-PULSE_PID=$!
-
-cvlc --play-and-exit -Z party.mp3
+gpio write 8 1
+aplay dj-sets/party.wav
 
 while true; do
 	gpio write 8 0
 
-	#VLC stutters in the beginning -> prepend 1s silence. play half of that now.
-	cvlc --play-and-exit -Z $(ls -x | grep short.mp3 | shuf -n 1)  & 
-	VLC_PID=$!
-	sleep 0.5
-	kill -s STOP $VLC_PID
-	
-	gpio wfi 7 falling
-	gpio write 8 1
-	sleep 0.25
+	while [[ "$(gpio read 7)" != "0" ]]; do
+		gpio wfi 7 falling
+		sleep 0.1
+	done	
 
-	kill -s CONT $VLC_PID
-	wait
-	sleep 0.5
+	gpio write 8 1
+	aplay dj-sets/current/$(ls -1 dj-sets/current/ | shuf -n 1)
 
 	if [[ "$(gpio read 7)" == "0" ]]; then
-		sudo systemctl poweroff;
-		cvlc --play-and-exit -Z poweroff.mp3
+		aplay dj-sets/poweroff.wav
+		systemctl poweroff
 	fi
 done
-
-kill $PULSE_PID
